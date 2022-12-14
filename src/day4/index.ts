919038c99fbc2e65be6e7e92d960a361be9ff1da
Pololops <paul.rigaudeau@me.com>
2022-12-14 20:52:46 +0100

    Day 4 fully completed

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -3,14 +3,6 @@ import * as fs from 'fs';
 
 const part1Result = 0
 
-const fakeData = `2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-6-6,4-6
-2-6,4-8
-`
-
 const app = {
   data: fs.readFileSync(path.join(__dirname, './data.txt'), 'utf-8'),
 
