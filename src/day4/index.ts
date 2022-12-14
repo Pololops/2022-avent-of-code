3fc6ef706fb01dcc896a29401469166e5d01bc06
Pololops <paul.rigaudeau@me.com>
2022-12-14 16:41:37 +0100

    Day 4 Part 1 : complete

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,11 +1,50 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-const data = fs.readFileSync(path.join(__dirname, './data.txt'), 'utf-8')
+const part1Result = 0
 
+const fakeData = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+`
 
+const app = {
+  data: fs.readFileSync(path.join(__dirname, './data.txt'), 'utf-8'),
+
+  formatData (data: string): number[][][] {
+    return data
+      .split('\n')
+      .filter((element) => element && element)
+      .map((element) => element
+        .split(',')
+        .map((subElement) => subElement
+        .split('-')
+        .map((string) => parseInt(string))
+      ))
+  },
+
+  getPairFullyRange (data: number[][][]): number[][][] {    
+    const foundPairs = data.filter((pair) => {
+      if (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]) return pair
+      if (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1]) return pair
+    })
+
+    return foundPairs
+  },
+
+  countPairFullyRange (data: number[][][]): number {
+    return data.length
+  }
+}
+
+const getResultOfPart1 = (data: string): number => {
+  return app.countPairFullyRange(app.getPairFullyRange(app.formatData(data)))
+}
 
 export default `
   DAY 4 :
-    
+    Number of pairs where one range fully contain the other: ${getResultOfPart1(app.data)}
 `
