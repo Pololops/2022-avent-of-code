e3fce36b8552c2cb8e2345a2adbddd6163dd1bc0
Pololops <paul.rigaudeau@me.com>
2022-12-15 00:22:00 +0100

    day 5 : data formatting

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -1,41 +1,85 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-const fakeData = ``
+const fakeData = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
 
 const app ={
   data: fs.readFileSync(path.join(__dirname, './data.txt'), 'utf-8'),
 
-  formatData (data: string): string[] {
-    return data
+  stacks: [] as string[],
+  procedure: [] as any[][],
+  stacksQuantity: 0,
+
+  formatData (data: string): void {
+    const [cratesAndStacks, procedure] = data.split('\n\n')
+
+    app.stacksQuantity = cratesAndStacks
       .split('\n')
-      .filter((element) => element && element)
-      //? if you want format more deeply
-      // .map((element) => element
-      //   .split(',')
-      //   .map((subElement) => subElement
-      //   .split('-')
-      //   .map((string) => parseInt(string))
-      // ))
+      .at(-1)?.split('')
+      .filter((char) => char !== ' ')
+      .length || 0
+
+    app.procedure = procedure
+      .split('\n')
+      .map((line) => line
+        .split('')
+        .filter((char) => /^[1-9]$/.test(char) && char)
+        .map((char) => parseInt(char))
+      )
+
+    const crates = cratesAndStacks
+      .split('\n')
+      .slice(0, -1)
+      .map((series) => series.split(' '))
+      // .filter((element, index) => {
+      //   if (element !== ' ') return element
+      // })
+    console.log(crates)
+    const stacks:string[] = []
+    crates.forEach((series, index) => {
+      if (index === 0 || index % 4 === 0) {
+        
+      }
+    })
+
+    // const arrayOfStacks = stacks.split('').filter((character) => {
+    //   if (character !== '[' && character !== ']' && character !== '\n') {
+    //     return character
+    //   }
+    // })
+
+
+
+
+    console.log('NB OF STACKS: ', app.stacksQuantity)
+    console.log('PROCEDURE: ', app.procedure)
+    console.log('CRATES: ', app.stacks)
   },
 
 
 }
 
-const getResultOfPart1 = (data: string): number => {
-  
-  
-  return 0;
+const getResultOfPart1 = (data: string): string => {
+  app.formatData(fakeData)
+
+  return 'TEST';
 }
 
 const getResultOfPart2 = (data: string): number => {
-  
-  
+
   return 0;
 }
 
 export default `
   DAY 5 :
-    Part 1: ${getResultOfPart1(fakeData)}
+    Crate ends up on top of each stack: ${getResultOfPart1(fakeData)}
     Part 2: ${getResultOfPart2(fakeData)}
 `
