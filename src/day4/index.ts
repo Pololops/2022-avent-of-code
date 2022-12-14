9e7650d65b9e1414ea67cfe7aa2f08d77d31829e
Pololops <paul.rigaudeau@me.com>
2022-12-14 20:52:22 +0100

    Day 4 fully completed

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -26,10 +26,19 @@ const app = {
       ))
   },
 
-  getPairFullyRange (data: number[][][]): number[][][] {    
+  getPairFullyOverlap (data: number[][][]): number[][][] {    
     const foundPairs = data.filter((pair) => {
       if (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]) return pair
       if (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1]) return pair
+    })
+
+    return foundPairs
+  },
+
+  getPairOverlap (data: number[][][]): number[][][] {    
+    const foundPairs = data.filter((pair) => {
+      if (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][0]) return pair
+      if (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][0]) return pair
     })
 
     return foundPairs
@@ -41,10 +50,16 @@ const app = {
 }
 
 const getResultOfPart1 = (data: string): number => {
-  return app.countPairFullyRange(app.getPairFullyRange(app.formatData(data)))
+  return app.countPairFullyRange(app.getPairFullyOverlap(app.formatData(data)))
+}
+
+
+const getResultOfPart2 = (data: string): number => {
+  return app.countPairFullyRange(app.getPairOverlap(app.formatData(data)))
 }
 
 export default `
   DAY 4 :
     Number of pairs where one range fully contain the other: ${getResultOfPart1(app.data)}
+    Number of pairs where ranges overlap: ${getResultOfPart2(app.data)}
 `
