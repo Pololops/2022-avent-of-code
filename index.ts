830946e5d4024e9d5a7529778fc934f02d351b76
Pololops <paul.rigaudeau@me.com>
2022-12-15 16:34:22 +0100

    Day 6 : ready

--- index.ts
+++ index.ts
@@ -5,9 +5,11 @@ import day2 from './src/day2'
 import day3 from './src/day3'
 import day4 from './src/day4'
 import day5 from './src/day5'
+import day6 from './src/day6'
 
 console.log(day1)
 console.log(day2)
 console.log(day3)
 console.log(day4)
 console.log(day5)
+console.log(day6)
