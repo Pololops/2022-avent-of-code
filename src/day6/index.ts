37be3fddfe5524fade9c4b5cfe71c9e040521e63
Pololops <paul.rigaudeau@me.com>
2022-12-18 17:56:06 +0100

    Day 6  : complete

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,80 +1,38 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-const fakeData = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`
+const fakeData = `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`
 
 const app = {
   data: fs.readFileSync(path.join(__dirname, './data.txt'), 'utf-8'),
 
   dataLength: 0,
 
-  formatData: (data: string): string[] => {
-    app.dataLength = data.length
-    const newData = []
-    
-    for (let i = 0; i < app.dataLength; i++) {
-      const word = data.substring(i - 3, i + 1)
-      if (word.length > 3) newData.push(word)
-    }
-    
-    return newData
+  formatData: (data: string, factor: number): string[] => {
+    return data.split('').map((_element, index, self) => {
+        return self.slice(index, index + factor).join('')
+    }).slice(0, data.length - (factor - 1))
   },
 
-  findMarker: (data: string[]): any => {
-
-    const indexes: number[] = []
-   
-    data.forEach((word, wordIndex) => {
-      const i =  word.split('').findIndex((item, index, self) => {
-        if (self.indexOf(item) != index) return wordIndex ;
-      })
-
-      if (i)
-
-      indexes.push(i)
+  findMarker: (data: string[], factor: number): number => {
+    const foundIndex = data.findIndex((element) => {
+      return !element
+        .split('')
+        .reverse()
+        .find((e, i, s) => s.includes(e, i + 1))    
     })
-
-    console.log(indexes)
-    return indexes
-
-
-
-    //const isOk: any = data.find((word, index) => !word
-    //    .includes(word[0], index)
-    //  )
-    //  // .findIndex((bool) => bool)
-//
-    //console.log(isOk)
-
-
-    // const result = data.filter((word) => {
-    //   const splittedWord = word.split('')
-// 
-    //   const found = splittedWord.filter((letter, index) => {
-    //     if (splittedWord.includes(letter, index + 1)) {}
-    //   })
-    //   return found
-    // })
-
-    //return app.dataLength + 1 - isOk
+      
+    return foundIndex + factor
   }
 }
 
-const getResultOfPart1 = (data: string): number => {
-  const formattedData = app.formatData(data)
-
-  console.log(formattedData)
-
-  return app.findMarker(formattedData)
-}
-
-const getResultOfPart2 = (data: string): number => {
-  
-  return 0;
+const getResults = (data: string, factor: number): number => {
+  const formattedData = app.formatData(data, factor)
+  return app.findMarker(formattedData, factor)
 }
 
 export default `
   DAY 6 :
-    Part 1: ${getResultOfPart1(fakeData)}
-    Part 2: ${getResultOfPart2(fakeData)}
+    Number of characters processed before the first detected start-of-packet marker: ${getResults(app.data, 4)}
+    Number of characters processed before the first detected start-of-message marker: ${getResults(app.data, 14)}
 `
